fb4101faae27c3720fcfde54a80442a7a74cfbf1
TTKKECHO <2396757940@qq.com>
2021-06-10 18:40:36 +0800

    扩展功能

--- src/deviceExtend.cpp
+++ src/deviceExtend.cpp
@@ -1,19 +1,129 @@
 #include "deviceExtend.h"
 
+ 
 
-int CapImg(int lUserID,int channel,char* src)
+hash_t hash_( char  const * str)         
+{         
+     hash_t ret{basis};      
+     while (*str){         
+         ret ^= *str;         
+         ret *= prime;      
+         str++;        
+     }    
+     return  ret;      
+}
+constexpr hash_t hash_compile_time( char  const * str, hash_t last_value )       
+{         
+     return  *str ? hash_compile_time(str+1, (*str ^ last_value) * prime) : last_value;        
+}
+
+constexpr unsigned  long  long  operator  ""  _hash( char  const * p,  size_t )        
+{   
+     return  hash_compile_time(p);      
+}
+
+void selectFun(Json::Value message,long user_id)
+{
+	switch(hash_(message["header"]["namespace"].asCString()))
+	{
+		case "xinke.device.query"_hash:		{	selectQueryFun		(message["header"]["name"].asString(),message["payload"],user_id);		}break;
+		case "xinke.device.upload"_hash:	{	selectUploadFun		(message["header"]["name"].asString(),message["payload"],user_id);		}break;
+		case "xinke.device.control"_hash:	{	selectControlFun	(message["header"]["name"].asString(),message["payload"],user_id);		}break;
+		case "xinke.device.setconfig"_hash:	{	selectSetConfigFun	(message["header"]["name"].asString(),message["payload"],user_id);		}break;
+		default:break;
+	}
+
+}
+
+void selectSetConfigFun(std::string name,Json::Value payload,long user_id)
+{
+	switch(hash_(name.c_str()))
+	{
+		case "device.loginfo"_hash:		{	setLogInfo		(payload);		}break;
+		case "device.capture"_hash:		{	setCapture		(payload);		}break;
+		case "device.remoteinfo"_hash:	{	setRemoteInfo	(payload);		}break;
+		case "device.projectinfo"_hash:	{	setProjectInfo	(payload);		}break;
+		
+		default:break;
+	}
+
+}
+
+void selectQueryFun(std::string name,Json::Value payload,long user_id)
+{
+
+}
+void selectUploadFun(std::string name,Json::Value payload,long user_id)
+{
+
+}
+void selectControlFun(std::string name,Json::Value payload,long user_id)
+{
+
+}
+
+
+int setLogInfo(Json::Value payload)
+{
+	Json::Value data = getConfig();
+	data[0]["device_ip"] = payload["device_ip"];
+	data[0]["username"] = payload["username"];
+    data[0]["password"] = payload["password"];
+    data[0]["port"] = payload["port"];
+	data[2]["device_id"] = payload["device_id"];
+	setConfig(data);
+	return 1;
+}
+
+int setCapture(Json::Value payload)
+{
+
+
+
+}
+int setRemoteInfo(Json::Value payload)
+{
+
+
+
+}
+int setProjectInfo(Json::Value payload)
+{
+
+
+
+}
+
+int CapImg(long user_id,int channel)
 {
 	NET_DVR_JPEGPARA strPicPara = {0};
     strPicPara.wPicQuality = 2;
-    strPicPara.wPicSize = 0;
-    int iRet;
-    iRet = NET_DVR_CaptureJPEGPicture(lUserID,channel, &strPicPara, src);
+    strPicPara.wPicSize =0;
+	char buffer[1920*1080];
+	LPDWORD size ;
+    bool iRet = NET_DVR_CaptureJPEGPicture_NEW(user_id,channel, &strPicPara, buffer,1920*1080,size);
+    // bool iRet = NET_DVR_CaptureJPEGPicture(user_id,channel, &strPicPara, "./Image/1.jpeg");
 	if (!iRet)
     {
-		printf("Cap ERROR!!!!!!!!!!!!!!!!");
+			printf("\nhere2!\n");
+
         printf("pyd1---NET_DVR_CaptureJPEGPicture error, %d\n", NET_DVR_GetLastError());
         return HPR_ERROR;
     }
+	//int len = (*size);
+	std::cout<<sizeof(LPDWORD)<<std::endl;
+	printf("\nhere2!\n");
+	std::string imgdata = base64_encode(buffer,352*288);
+	Json::Value config = getConfig();
+    RAMQ request(config[1]);
+	request.exchange = config[1]["exchange"].asString();
+	request.key=config[1]["key"].asString();
+	request.connect(config[1]);
+    request.open_channel(CH_RECV);
+    request.open_channel(CH_UPLOAD);
+    request.upload_queue = config[1]["upload_queue"].asString();
+	request.message = imgdata ;
+	request.publish(imgdata);
 	return 0;
 }
 
--- src/deviceExtend.h
+++ src/deviceExtend.h
@@ -1,16 +1,43 @@
 #ifndef __DEVICEEXTEND_H
-#define __CAPPI__DEVICEEXTEND_HCTURE_H	 
+#define __DEVICEEXTEND_H
 
 #include <iostream>
 #include <string>
 #include <stdio.h>
 #include <unistd.h>
+#include <fstream>
 #include "HCNetSDK.h"
+#include "Common.h"
+#include <jsoncpp/json/json.h>
+#include "base64.h"
+#include "rabbitmq.h"
+
+typedef  std::uint64_t hash_t;  
+constexpr hash_t basis = 0xCBF29CE484222325ull;        
+constexpr hash_t prime = 0x100000001B3ull;         
+
+hash_t hash_( char  const * str);
+constexpr hash_t hash_compile_time( char  const * str, hash_t last_value = basis);
+constexpr unsigned  long  long  operator  ""  _hash( char  const * p,  size_t );
 
 
-int CapImg(int lUserID,int channel,char* src);
+int CapImg(long lUserID,int channel);
 void zoomin(int lUserID,int channel,int sleep_time=10000);
 void zoomout(int lUserID,int channel,int sleep_time=10000);
+
+void selectFun(Json::Value message,long user_id);
+void selectQueryFun(std::string name,Json::Value payload,long user_id);
+void selectUploadFun(std::string name,Json::Value payload,long user_id);
+void selectControlFun(std::string name,Json::Value payload,long user_id);
+void selectSetConfigFun(std::string name,Json::Value payload,long user_id);
+
+int setLogInfo(Json::Value payload);
+int setCapture(Json::Value payload);
+int setRemoteInfo(Json::Value payload);
+int setProjectInfo(Json::Value payload);
+
+
+
 
 
 
