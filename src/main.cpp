b3ba23acc2d93935d48c4cfd4745938c6950b83e
TTKKECHO <2396757940@qq.com>
2021-07-08 15:48:31 +0800

    main文件

--- src/main.cpp
+++ src/main.cpp
@@ -6,41 +6,53 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+// #include <amqp.h>
+// #include <amqp_framing.h>
+#include "rabbitmq.h"
+#include "deviceExtend.h"
 #include "Common.h"
 #include "HCNetSDK.h"
-#include "http_libcurl.h"
 #include "base64.h"
 #include "alarm.h"
 #include "facelib.h"
 #include "background.h"
-#include <amqp.h>
-#include <amqp_framing.h>
-#include "rabbitmq.h"
-#include "deviceExtend.h"
+// #include "http_libcurl.h"
 
 
 
 using namespace std;
 DEVICE_INFO device_info;
 
+
+
 int main()
  {
-	
+	//获取配置文件
 	Json::Value config = getConfig();
+	//注册超脑设备
 	long user_id = NVR_Init(config[0]);
-	device_info.user_id = user_id;
+	//读取配置文件中设备编号
 	device_info.device_id=config[2]["device_id"].asString();
-	if(user_id < 0)
-	{
-		printf("\n[ERROR]设备初始化失败！\n");
-		return 0;
-	}
-	get_FDLib_capabilities(user_id);
-	CapImg(33);
-
-	//创建报警布防线程与网络通信线程
-	thread alarm(thread_ALARM,user_id);
+	//开启rabbitmq后台监听线程
 	thread ramq(thread_RAMQ);
+	//检查超脑设备是否注册成功
+	while(user_id < 0)			//小于0说明注册失败
+	{
+		//上传离线报警信息
+		uploadWarnning("offline");
+		printf("\n[ERROR]设备初始化失败！\n");
+		//等待30s重新注册设备
+		sleep(30);
+		config = getConfig();
+		user_id = NVR_Init(config[0]);
+	}
+	//获取用户ID号
+	device_info.user_id = user_id;
+
+	//创建报警布防线程
+	thread alarm(thread_ALARM,user_id);
+
+	//等待报警、通信线程结束
 	alarm.join();
 	ramq.join();
 
